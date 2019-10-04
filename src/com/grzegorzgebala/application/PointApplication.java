fcfed7877061c65c48735eb9e9705584a1944ba3
Grzegorz <grzegorz.gebala@yahoo.com>
2019-10-04 23:17:23 +0200

    Initial point application

---
+++ src/com/grzegorzgebala/application/PointApplication.java
@@ -0,0 +1,21 @@
+package com.grzegorzgebala.application;
+
+import com.grzegorzgebala.methods.PointController;
+import com.grzegorzgebala.objects.Point;
+
+public class PointApplication {
+    public static void main(String[] args) {
+        Point p1 = new Point(10, 20);
+        PointController pc = new PointController();
+
+        System.out.println("Punkt: (" + p1.getX()+";"+p1.getY()+")");
+        pc.addX(p1);
+        System.out.println("Punkt addX: (" + p1.getX()+";"+p1.getY()+")");
+        pc.addY(p1);
+        System.out.println("Punkt addY: (" + p1.getX()+";"+p1.getY()+")");
+        pc.minusX(p1);
+        System.out.println("Punkt minusX: (" + p1.getX()+";"+p1.getY()+")");
+        pc.minusY(p1);
+        System.out.println("Punkt minusY: (" + p1.getX()+";"+p1.getY()+")");
+    }
+}
---
+++ src/com/grzegorzgebala/methods/PointController.java
@@ -0,0 +1,22 @@
+package com.grzegorzgebala.methods;
+
+import com.grzegorzgebala.objects.Point;
+
+public class PointController {
+
+    public void addX(Point p) {
+        p.setX(p.getX() + 1);
+    };
+
+    public void minusX(Point p) {
+        p.setX(p.getX() - 1);
+    };
+
+    public void addY(Point p) {
+        p.setY(p.getY() + 1);
+    };
+
+    public void minusY(Point p) {
+        p.setY(p.getY() - 1);
+    };
+}
